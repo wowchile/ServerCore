92c4aa64f83f455cac1835c50c2325f8305c94f0
José González <maangx@gmail.com>
2018-12-27 21:26:45 -0300

    [Core/Raid] Add missing SQL in 5a45fbebd9d654faafbde80abd898a5df4d8b292

---
+++ data/sql/updates/db_world/2018_12_27_00_creature_template.sql
@@ -0,0 +1,5 @@
+-- Script for Living Inferno (Halion fight)
+UPDATE `creature_template` SET `ScriptName`='npc_living_inferno' WHERE `entry` = 40681;
+
+-- Script for Living Ember (Halion fight)
+UPDATE `creature_template` SET `ScriptName`='npc_living_ember' WHERE  `entry` = 40683;
