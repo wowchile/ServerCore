ee9171a07ad57212faab00b4efbd1f86b55fc595
José González <maangx@gmail.com>
2018-12-30 01:38:45 -0300

    [Core/Spells] Add internal cooldown for enchant Spellsurge

---
+++ data/sql/updates/db_world/2018_12_30_00_spell_proc_event.sql
@@ -0,0 +1,2 @@
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES 
+(27997, 0, 0, 0, 0, 0, 0, 0, 0, 0, 30000);
