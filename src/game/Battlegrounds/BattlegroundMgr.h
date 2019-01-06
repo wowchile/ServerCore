89fb4782f0561e5eff9460a0353106707093f278
José González <maangx@gmail.com>
2019-01-06 14:35:21 -0300

    typo x2

--- src/game/Battlegrounds/BattlegroundMgr.h
+++ src/game/Battlegrounds/BattlegroundMgr.h
@@ -140,7 +140,7 @@ class BattlegroundMgr
                 Battleground* bg = itr->second;
                 if (bg->GetBgTypeID() == bgTypeId)
                 {
-                    filteredBgs[bg->GetInstanceID] = bg;
+                    filteredBgs[bg->GetInstanceID()] = bg;
                 }
             }
 
